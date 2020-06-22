64e36a101c025cbeb15c77b05344e0eed1e30a55
Tom Viner <tom@viner.tv>
2020-06-22 23:39:37 +0100

    add build, for github-pages

---
+++ build/precache-manifest.af2811eca300232b4fbcc0b02ad7b74c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affe50487b40706a3d24b6128a251aa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd2db225fb442f33ce1",
+    "url": "/static/css/main.b5ac138b.chunk.css"
+  },
+  {
+    "revision": "dae268075543dd02b662",
+    "url": "/static/js/2.c32199c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c32199c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd2db225fb442f33ce1",
+    "url": "/static/js/main.5ce56b7d.chunk.js"
+  },
+  {
+    "revision": "1e814049eb6d0e6e4b59",
+    "url": "/static/js/runtime-main.5f3b2643.js"
+  }
+]);
