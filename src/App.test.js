0b0264058a3eb9bd75b55746a305a1524cb72ae6
Tom Viner <tom@viner.tv>
2020-06-26 19:44:49 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders cells', () => {
+test('renders app', () => {
   const { getAllByText } = render(<App />);
-  const cells = getAllByText('go');
+  const cells = getAllByText('+');
   cells.forEach(cell => expect(cell).toBeInTheDocument());
 });
